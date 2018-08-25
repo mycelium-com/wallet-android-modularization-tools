c94bd25e2edc9d8181a2d8419501fe7cbc743fbd
Leo Wandersleb <leo@leowandersleb.de>
2018-08-25 17:00:49 -0300

    moved library into a containing project
    now the library is a module in a containing project
    two standard android apps, Hello and world were created
    new apps will get modularized

---
+++ modularization-lib/src/androidTest/java/com/mycelium/wallet_android_modularization_tools/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.mycelium.wallet_android_modularization_tools;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.mycelium.wallet_android_modularization_tools.test", appContext.getPackageName());
+    }
+}
