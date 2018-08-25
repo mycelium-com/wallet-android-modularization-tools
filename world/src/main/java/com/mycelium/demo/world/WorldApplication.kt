12dd598f5b777039b8c0ce0ec43def2cd819d961
Leo Wandersleb <leo@leowandersleb.de>
2018-08-25 18:49:17 -0300

    adding pairing request
    adding pairing request to the application onCreate. This request will
    get ignored by the reqpective other app, as the signature is not right
    yet. You have to check logcat with the other app to see the actual
    signature which is different than the expected signature:
    java.lang.SecurityException: Signature F6/t7Bj0qtPDmfNdjqReSIHxR6lrwNxaHtT5go6f7HI= can't be verified for package com.mycelium.demo.world.

--- world/src/main/java/com/mycelium/demo/world/WorldApplication.kt
+++ world/src/main/java/com/mycelium/demo/world/WorldApplication.kt
@@ -1,17 +1,17 @@
-package com.mycelium.demo.hello
+package com.mycelium.demo.world
 
 import android.app.Application
 import android.util.Log
 import com.mycelium.modularizationtools.CommunicationManager
 
-class HelloApplication : Application() {
+class WorldApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         CommunicationManager.init(this)
         try {
-            CommunicationManager.getInstance().requestPair("com.mycelium.demo.world")
+            CommunicationManager.getInstance().requestPair("com.mycelium.demo.hello")
         } catch (se: SecurityException) {
-            Log.w("HelloApplication", se.message)
+            Log.w("WorldApplication", se.message)
         }
     }
 }
